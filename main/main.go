bb859f7b3f7e8c3adfe317d3369746d7c424ba27
axel <axel@familjenwallin.se>
2017-09-11 11:17:26 +0200

    Added routing table in main by randomizing 100 contacts. Did a FindClosestContacts like in the test code which finds 20 contacts.

--- main/main.go
+++ main/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-		"fmt"
-		sample "D7017e-Kademlia/SampleCode"
+	"fmt"
+	"strconv"
+	"strings"
+	sample "D7017e-Kademlia/SampleCode"
 )
 
 func main() {
-    fmt.Printf("hello, world\n")
-    rt := sample.NewContact(sample.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000")
-    fmt.Printf(rt.String())
+	rt := sample.NewRoutingTable(sample.NewContact(sample.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+
+	for i := 1; i < 100; i++ {
+		port := 8000 + i
+		s := []string{}
+		s = append(s, "localhost:")
+		s = append(s, strconv.Itoa(port))
+		a := strings.Join(s, "")
+		rt.AddContact(sample.NewContact(sample.NewKademliaID(sample.NewRandomKademliaID().String()), a))
+	}
+
+	contacts := rt.FindClosestContacts(sample.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), 20)
+	for i := range contacts {
+		fmt.Println(contacts[i].String())
+	}
 }
